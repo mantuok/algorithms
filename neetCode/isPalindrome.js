602ef74c323ffb8a495162c4dc0292e79731aef9
mantuok <mantuok@gmail.com>
2024-09-27 12:32:00 -0700

    ..

---
+++ neetCode/isPalindrome.js
@@ -0,0 +1,23 @@
+const isPalindrome = (s) => {
+  if (s.length <= 1) return true;
+
+  let [left, right] = [0, s.length - 1];
+  let leftChar, rightChar;
+
+  while (left < right) {
+    leftChar = s[left];
+    rightChar = s[right];
+    if (!/[a-zA-Z0-9]/.test(leftChar)) {
+      left++;
+    } else if (!/[a-zA-Z0-9]/.test(rightChar)) {
+      right--;
+    } else {
+      if (leftChar.toLowerCase() !== rightChar.toLowerCase()) return false;
+      left++;
+      right--;
+    }
+  }
+  return true;
+}
+
+console.log(isPalindrome("Was it a car or a cat I saw?"))
